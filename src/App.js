8c19f07e4eeb97b2e6ac79d2be9336d9f8baef43
ollie314 <mehdi.lefebvre@gmail.com>
2021-05-18 14:51:40 +0200

    chore: relocate style
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
 import BlogAppBar from './BlogAppBar';
+
+import { useStyles } from './style';
 
 const Copyright = () => {
   return (
@@ -25,38 +26,6 @@ const Copyright = () => {
     </Typography>
   );
 };
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
