5bde27abe26e6862d270f5820ca8dc23a7f356db
ollie314 <mehdi.lefebvre@gmail.com>
2021-05-18 14:58:20 +0200

    chore: relocate banner
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Container from '@material-ui/core/Container';
 
 import BlogAppBar from './BlogAppBar';
 import Copyright from './Copyright';
+import Banner from './Banner';
+
 import { useStyles } from './style';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -22,35 +24,8 @@ function App() {
       <CssBaseline />
       <BlogAppBar classes={classes} title={"Album Sample"} />
       <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
+        <Banner classes={classes} />
         <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
